2aa4ae7c314f5994cd00ff26ed1cecf7af52c011
Filip Defar <filip.defar@toptal.com>
2017-03-17 13:30:07 +0100

    WIP: Animated more parts of the graph

--- src/percent_widget.js
+++ src/percent_widget.js
@@ -7,8 +7,8 @@ class PercentWidget {
     this.thickness = options.thickness || 40;
     this.innerRadius = this.radius - this.thickness;
 
-    const borderThickness = options.borderThickness || 5;
-    const pointerSize = options.pointerSize || 6;
+    this.borderThickness = options.borderThickness || 5;
+    this.pointerSize = options.pointerSize || 6;
     const labelSize = options.labelSize || 25;
 
     const borderColor = options.borderColor || '#69D2E7';
@@ -16,11 +16,11 @@ class PercentWidget {
     const backgroundColor = options.backgroundColor || '#eee';
     const fontColor = options.fontColor || '#666';
 
-    const width = (this.radius + borderThickness) * 2;
-    const height = width;
+    this.width = (this.radius + this.borderThickness) * 2;
+    this.height = this.width;
 
     const parent = d3.select(parentSelector);
-    const canvas = parent.append('svg').attr('width', width).attr('height', height);
+    const canvas = parent.append('svg').attr('width', this.width).attr('height', this.height);
 
     let backgroundArc = d3.arc()
       .startAngle(0)
@@ -29,7 +29,7 @@ class PercentWidget {
       .outerRadius(this.radius);
 
     canvas.append('path')
-      .attr('transform', `translate(${width/2}, ${height/2})`)
+      .attr('transform', `translate(${this.width/2}, ${this.height/2})`)
       .attr('d', backgroundArc)
       .attr('fill', backgroundColor);
 
@@ -40,43 +40,43 @@ class PercentWidget {
       .outerRadius(this.radius);
 
     this.mainArcPath = canvas.append('path')
-      .attr('transform', `translate(${width/2}, ${height/2})`)
+      .attr('transform', `translate(${this.width/2}, ${this.height/2})`)
       .attr('d', mainArc)
       .attr('fill', fillColor);
 
     let arcBorder = d3.arc()
       .startAngle(0)
       .endAngle(2 * Math.PI * percentage / 100)
-      .innerRadius(this.radius - borderThickness / 2)
-      .outerRadius(this.radius + borderThickness / 2);
+      .innerRadius(this.radius - this.borderThickness / 2)
+      .outerRadius(this.radius + this.borderThickness / 2);
 
-    canvas.append('path')
-      .attr('transform', `translate(${width/2}, ${height/2})`)
+    this.arcBorderPath = canvas.append('path')
+      .attr('transform', `translate(${this.width/2}, ${this.height/2})`)
       .attr('d', arcBorder)
       .attr('fill', borderColor);
 
     canvas.append('circle')
       .attr('cx', 0)
       .attr('cy', -this.radius)
-      .attr('r', borderThickness / 2)
+      .attr('r', this.borderThickness / 2)
       .attr('fill', borderColor)
-      .attr('transform', `translate(${width/2}, ${height/2})`);
+      .attr('transform', `translate(${this.width/2}, ${this.height/2})`);
 
     canvas.append('circle')
       .attr('cx', 0)
       .attr('cy', -this.radius)
-      .attr('r', borderThickness / 2)
+      .attr('r', this.borderThickness / 2)
       .attr('fill', borderColor)
-      .attr('transform', `translate(${width/2}, ${height/2}) rotate(${360 * percentage / 100})`);
+      .attr('transform', `translate(${this.width/2}, ${this.height/2}) rotate(${360 * percentage / 100})`);
 
-    canvas.append('polygon')
-      .attr('points', `0,-${pointerSize} ${pointerSize},${pointerSize}, -${pointerSize},${pointerSize}`)
+    this.pointer = canvas.append('polygon')
+      .attr('points', `0,-${this.pointerSize} ${this.pointerSize},${this.pointerSize}, -${this.pointerSize},${this.pointerSize}`)
       .attr('fill', borderColor)
-      .attr('transform', `translate(${width/2}, ${height/2}) rotate(${360 * percentage / 100}) translate(0, -${this.innerRadius+pointerSize})`);
+      .attr('transform', `translate(${this.width/2}, ${this.height/2}) rotate(${360 * percentage / 100}) translate(0, -${this.innerRadius+this.pointerSize})`);
 
     canvas.append('text')
-      .attr('x', width / 2)
-      .attr('y', height / 2 + labelSize / 2)
+      .attr('x', this.width / 2)
+      .attr('y', this.height / 2 + labelSize / 2)
       .attr('text-anchor', 'middle')
       .attr('font-family', 'verdana')
       .attr('font-size', `${labelSize}px`)
@@ -88,7 +88,11 @@ class PercentWidget {
     let innerRadius = this.innerRadius;
     let radius = this.radius;
     let oldPercentage = this.percentage;
+    let borderThickness = this.borderThickness;
     let tweenFunction = this._arcTween;
+    let pointerSize = this.pointerSize;
+    let width = this.width;
+    let height = this.height;
 
     const duration = 1500;
 
@@ -101,6 +105,26 @@ class PercentWidget {
           .innerRadius(innerRadius)
           .outerRadius(radius);
         return tweenFunction(newArc, oldPercentage, newPercentage);
+      });
+
+    this.arcBorderPath
+      .transition()
+      .duration(duration)
+      .attrTween('d', function() {
+        var newArcBorder = d3.arc()
+          .startAngle(0)
+          .innerRadius(radius - borderThickness / 2)
+          .outerRadius(radius + borderThickness / 2);
+        return tweenFunction(newArcBorder, oldPercentage, newPercentage);
+      });
+
+    this.pointer
+      .transition()
+      .duration(duration)
+      .attrTween('transform', function() {
+        return function(t) {
+          return `translate(${width/2}, ${height/2}) rotate(${360 * (oldPercentage + (newPercentage - oldPercentage) * t) / 100}) translate(0, -${innerRadius+pointerSize})`;
+        }
       });
   }
 
