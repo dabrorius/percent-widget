346d145b55209a9ed3cc8840a3f31c2b59f869ec
Filip Defar <filip.defar@toptal.com>
2017-03-20 14:18:39 +0100

    Bind PercentWidget to window

--- src/app.js
+++ src/app.js
@@ -1,13 +1,4 @@
 const d3 = require('d3');
 const PercentWidget = require('./percent_widget');
 
-window.widget = new PercentWidget('#percent-widget');
-widget.update(90);
-
-window.widgetRed = new PercentWidget('#percent-widget-red', 90, {borderColor: '#FF0000', thickness: 50, fillColor: '#FFaaaa', pointerSize: 3, borderThickness: 3});
-widget.update(30);
-
-window.widgetSmall = new PercentWidget('#percent-widget-small', 0, {radius: 50, thickness: 20, labelSize: 16});
-widget.update(60);
-
-module.exports = PercentWidget;
+window.PercentWidget = PercentWidget;
