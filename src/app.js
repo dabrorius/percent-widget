0e03e30f66dfda70b567f4f61fedfbda5fad30b8
Filip Defar <filip.defar@toptal.com>
2017-03-20 14:12:55 +0100

    Implement update button

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const d3 = require('d3');
 const PercentWidget = require('./percent_widget');
 
-var widget = new PercentWidget('#percent-widget');
+window.widget = new PercentWidget('#percent-widget');
 widget.update(90);
 
-var widget = new PercentWidget('#percent-widget-red', 90, {borderColor: '#FF0000', fillColor: '#FFaaaa', pointerSize: 3, borderThickness: 3});
+window.widgetRed = new PercentWidget('#percent-widget-red', 90, {borderColor: '#FF0000', thickness: 50, fillColor: '#FFaaaa', pointerSize: 3, borderThickness: 3});
 widget.update(30);
 
-var widget = new PercentWidget('#percent-widget-small', 0, {radius: 50, thickness: 20, labelSize: 16});
+window.widgetSmall = new PercentWidget('#percent-widget-small', 0, {radius: 50, thickness: 20, labelSize: 16});
 widget.update(60);
+
+module.exports = PercentWidget;
